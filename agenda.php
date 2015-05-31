7f22b12cfab271f08f4906e4054f4e34ae2cf3a7
orifichu <orifichu@gmail.com>
2015-05-30 22:52:08 -0500

    Avances Agenda
    Avances en la creación del archivo agenda.php el cual mostrará las
    videoconferencias a realizarse en los distintos equipos Polycom en
    determinado día.

--- agenda.php
+++ agenda.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Agéndame</title>
+        <title>Agenda | Agéndame</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -20,14 +20,15 @@
         </style>
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/main.css">
+        <link id="bsdp-css" href="css/datepicker3.css" rel="stylesheet">
 
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
-    <body class="home">
+    <body class="agenda">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <!--nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -36,48 +37,31 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Agéndame</a>
+          <a class="navbar-brand" href="index.php">Agéndame</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right" role="form">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
-        </div--><!--/.navbar-collapse -->
-      <!--/div>
-    </nav-->
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <!--div class="jumbotron">
-      <div class="container">
-        <h1>Agéndame</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Inicio</a></li>
+            <li class="active"><a href="agenda.php">Agenda</a></li>
+          </ul>
+        </div>
       </div>
-    </div-->
+    </nav>
 
     <!-- Part 1: Wrap all page content here -->
     <div id="wrap">
 
       <!-- Begin page content -->
       <div class="container">
-        <div class="row text-center">
-          <div class="col-md-12">
-            <h1>Agéndame</h1>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="datepicker"></div>
           </div>
-        </div>
-        <div class="row text-center">
-          <div class="col-md-4 col-md-push-4">
-            <p class="subtitle">Pertenezco a:</p>
-            <p><a class="btn btn-primary btn-lg btn-block" href="#" role="button">Corte de Lambayeque</a></p>
-            <p><a class="btn btn-default btn-lg btn-block" href="#" role="button">Otra Corte</a></p>
-            <hr/>
-            <p><a class="btn btn-default btn-lg btn-block" href="agenda.php" role="button">Ver Agenda</a></p>
+          <div class="col-md-4">
+            <p class="subtitle">Polycom 1<br />(Nueva Sede)</p>
+          </div>
+          <div class="col-md-4">
+            <p class="subtitle">Polycom 1<br />(Penal de Chiclayo)</p>
           </div>
         </div>
       </div>
@@ -91,6 +75,9 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
         <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/vendor/bootstrap-datepicker.js"></script>
+        <script src="js/vendor/bootstrap-datepicker.es.js" charset="UTF-8"></script>
 
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$( document ).ready(function() {
+  // Handler for .ready() called.
+	$('div.datepicker').datepicker({
+		format: "dd/mm/yy",
+		language: "es"
+	});
+});
